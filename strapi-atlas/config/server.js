400138fbd324e27214cb28b1cb563744006ea94a
Shafeef <76443472+shfeefrazak@users.noreply.github.com>
2021-09-12 11:00:26 +0400

    12/19\21

---
+++ strapi-atlas/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39f956f3f6647d43645aa155aa879793'),
+    },
+  },
+});
---
+++ strapi-atlas/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a7a9192-6869-4c65-bc3e-b5ecadf272e0'
+};
